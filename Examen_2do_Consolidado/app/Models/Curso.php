194b7edc1da3f616949a0bb35abd300685a1739f
Jose Hernando Lopez <kota012011@gmail.com>
2025-04-10 19:49:27 +0000

    6- configurando los Model (estudiante-curso-inscrp#

--- Examen_2do_Consolidado/app/Models/Curso.php
+++ Examen_2do_Consolidado/app/Models/Curso.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable =['nombre','descripcion','duracion'];
+
+    public function estudiante ()
+    {
+        return $this->belongsToMany(Estudiante::class, 'inscripciones')
+                    ->withPivot('fecha_inscripcion')
+                    ->withTimestamps();
+    }
 }
--- Examen_2do_Consolidado/app/Models/Estudiante.php
+++ Examen_2do_Consolidado/app/Models/Estudiante.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Estudiante extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre ',
+                            'apellido',
+                            'fecha_nacimiento',
+                            'email'];
+
+    public function cursos()
+    {
+        return $this->belongsToMany(Curso :: class, 'inscripciones')
+                    ->withPivot('fecha_inscripcion')
+                    ->withTimestamps();
+    }
 }
