21840bdb69002652676752e953b8932bd6966371
joseLopez <kota012011@gmail.com>
2025-04-11 17:32:48 -0500

    8- Configurando las Rutas

--- Examen_2do_Consolidado/routes/web.php
+++ Examen_2do_Consolidado/routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\InscripcionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('estudiantes', EstudianteController::class);
+    Route::resource('cursos', CursoController::class);
+    Route::resource('inscripciones', InscripcionController::class);
+
+    Route ::get ('estudiantes/{estudiante}/cursos',[EstudianteController::class, 'showCursos'])->name('estidnates.cursos');
+
+    Route ::get ('cursos/{curso}/estudiantes',[CursoController::class, 'showEstudiantes'])->name('cursos.estudiantes');
+
 });
 
 require __DIR__.'/auth.php';
